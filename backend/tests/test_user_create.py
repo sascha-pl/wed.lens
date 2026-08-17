abf283b64ba5751a0a2336b4019269942d862265
Sascha Pleßberger <plessberger@black.com>
2026-08-17 18:17:42 +0200

    user create backend + tests. argon2id installed for hashing

---
+++ backend/tests/test_user_create.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_api_user_create() -> None:
+    response = client.post(
+        "/api/user_create",
+        json={
+            "name": "John Doe",
+            "email": "john@example.com",
+            "password": "secret-password",
+        },
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["sucess"] == True
+    assert "password" not in data
+    assert "password_hash" not in data
